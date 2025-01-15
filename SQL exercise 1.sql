ce61d5ec50afebd139f330ba7088bb1c8928f3c3
Harshk116 <harsh.kadam@mobiuso.com>
2025-01-15 15:15:28 +0530

    Update SQL exercise 1 .sql

--- SQL exercise 1.sql
+++ SQL exercise 1.sql
@@ -3,3 +3,5 @@ SHOW TABLES
 FROM sakila;
 -- Q2: Display the structure of the table “actor”
 DESCRIBE sakila.actor;
+-- Q3: Display the schema which was used to create the table “actor”
+SHOW CREATE TABLE sakila.actor;
