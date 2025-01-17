701ff60e03819e70c24f1676a114cfcac381d778
Harshk116 <harsh.kadam@mobiuso.com>
2025-01-17 11:00:33 +0530

    Update SQL exercise 2.sql

--- SQL exercise 2.sql
+++ SQL exercise 2.sql
@@ -2,3 +2,7 @@
 SELECT Title 
 FROM Album 
 WHERE AlbumId = 67;
+-- Q2: Find the name and length (in seconds) of all tracks that have a length between 50 and 70 seconds.
+SELECT Name, Milliseconds / 1000 AS LengthInSeconds 
+FROM Track 
+WHERE Milliseconds / 1000 BETWEEN 50 AND 70;
