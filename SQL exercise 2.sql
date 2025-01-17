02877f8c7bd14839fb6f4cfdb70356dcc8fafb6c
Harshk116 <harsh.kadam@mobiuso.com>
2025-01-17 16:11:54 +0530

    Update SQL exercise 2.sql

--- SQL exercise 2.sql
+++ SQL exercise 2.sql
@@ -81,3 +81,7 @@ JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
 GROUP BY FullName
 ORDER BY SUM(Invoice.Total) DESC
 LIMIT 1;
+-- Q.5 Provide a query showing Customers (just their full names, customer ID, and country) who are not in the US.
+SELECT Customer.FirstName || ' ' || Customer.LastName AS FullName, Customer.CustomerId, Customer.Country
+FROM Customer
+WHERE Customer.Country != 'USA';
