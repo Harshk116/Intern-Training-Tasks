d08c1e9fac9770f3a7cb334260587ee3fde44db9
Harshk116 <harsh.kadam@mobiuso.com>
2025-01-21 02:13:49 +0530

    Update SQL Exercise 3.sql

--- SQL Exercise 3.sql
+++ SQL Exercise 3.sql
@@ -11,3 +11,25 @@ SELECT
     END AS updated_price
 FROM product
 ORDER BY product_class_code DESC;
+-- 2.  Write a query to display (product_class_desc, product_id, product_desc, product_quantity_avail ) and Show inventory status of products as below as per their available quantity: a. For Electronics and Computer categories, if available quantity is <= 10, show 'Low stock', 11 <= qty <= 30, show 'In stock', >= 31, show 'Enough stock' b. For Stationery and Clothes categories, if qty <= 20, show 'Low stock', 21 <= qty <= 80, show 'In stock', >= 81, show 'Enough stock' c. Rest of the categories, if qty <= 15 – 'Low Stock', 16 <= qty <= 50 – 'In Stock', >= 51 – 'Enough stock' For all categories, if available quantity is 0, show 'Out of stock'. Hint: Use case statement. (60 ROWS) [NOTE: TABLES TO BE USED – product, product_class]
+SELECT 
+    pc.product_class_desc,
+    p.product_id,
+    p.product_desc,
+    p.product_quantity_avail,
+    CASE 
+        WHEN pc.product_class_desc IN ('Electronics', 'Computer') AND p.product_quantity_avail = 0 THEN 'Out of stock'
+        WHEN pc.product_class_desc IN ('Electronics', 'Computer') AND p.product_quantity_avail <= 10 THEN 'Low stock'
+        WHEN pc.product_class_desc IN ('Electronics', 'Computer') AND p.product_quantity_avail BETWEEN 11 AND 30 THEN 'In stock'
+        WHEN pc.product_class_desc IN ('Electronics', 'Computer') AND p.product_quantity_avail >= 31 THEN 'Enough stock'
+        WHEN pc.product_class_desc IN ('Stationery', 'Clothes') AND p.product_quantity_avail = 0 THEN 'Out of stock'
+        WHEN pc.product_class_desc IN ('Stationery', 'Clothes') AND p.product_quantity_avail <= 20 THEN 'Low stock'
+        WHEN pc.product_class_desc IN ('Stationery', 'Clothes') AND p.product_quantity_avail BETWEEN 21 AND 80 THEN 'In stock'
+        WHEN pc.product_class_desc IN ('Stationery', 'Clothes') AND p.product_quantity_avail >= 81 THEN 'Enough stock'
+        WHEN p.product_quantity_avail = 0 THEN 'Out of stock'
+        WHEN p.product_quantity_avail <= 15 THEN 'Low stock'
+        WHEN p.product_quantity_avail BETWEEN 16 AND 50 THEN 'In stock'
+        WHEN p.product_quantity_avail >= 51 THEN 'Enough stock'
+    END AS inventory_status
+FROM product p
+JOIN product_class pc ON p.product_class_code = pc.product_class_code;
